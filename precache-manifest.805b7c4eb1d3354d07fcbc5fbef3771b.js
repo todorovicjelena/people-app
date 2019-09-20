875f4d7f860fa54222d516f7ac1ac9ac56cbb975
pt <predrag@pts-MacBook-Pro.local>
2019-09-20 18:12:22 +0200

    Updates

---
+++ precache-manifest.805b7c4eb1d3354d07fcbc5fbef3771b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2abdf87cae9ea7ddfcaf23e6b8df9377",
+    "url": "/people-app/index.html"
+  },
+  {
+    "revision": "d2123db8ebcc46f302d0",
+    "url": "/people-app/static/css/main.70709f4f.chunk.css"
+  },
+  {
+    "revision": "14831ee6f2617c9189f6",
+    "url": "/people-app/static/js/2.ef8f9838.chunk.js"
+  },
+  {
+    "revision": "d2123db8ebcc46f302d0",
+    "url": "/people-app/static/js/main.22a5dcab.chunk.js"
+  },
+  {
+    "revision": "74339fe66b84111b5714",
+    "url": "/people-app/static/js/runtime-main.d6958929.js"
+  }
+]);
